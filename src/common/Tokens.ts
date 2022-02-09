ba232aed794e3ab40bd5fa8e434459e24649e326
Emanuel Martinez <me@manumartinez.es>
2022-02-09 15:52:40 +0100

    Configure initial tokens

---
+++ src/common/Tokens.ts
@@ -0,0 +1,19 @@
+export const Tokens = {
+  Colors: {
+    Buttons: {
+      Primary: "#fff",
+      Secondary: "#000",
+    },
+  },
+  Borders: {
+    Radius: {},
+  },
+  Constraints: {
+    Margin: {
+      XSmall: "",
+      Small: "",
+      MediumSmall: "",
+    },
+    Padding: {},
+  },
+};
--- src/components/Buttons/Primary/ButtonPrimaryStyled.ts
+++ src/components/Buttons/Primary/ButtonPrimaryStyled.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components";
+import { Tokens } from "../../../common/Tokens";
 
 // * Styled primary button
 export const PrimaryButtonStyled = styled.button`
   border: none;
-  background-color: #fff;
+  background-color: ${Tokens.Colors.Buttons.Primary};
   color: #000;
 `;
