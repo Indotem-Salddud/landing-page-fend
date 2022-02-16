ac67ddfccc8c10a8b174efa7680e69ede187ac19
Alcazar87 <carlosalcazar@indotem.com>
2022-02-16 19:00:53 +0100

    Add hover efect

--- src/components/Cards/Employee_card/EmployeeCardStyled.ts
+++ src/components/Cards/Employee_card/EmployeeCardStyled.ts
@@ -37,6 +37,25 @@ top: calc(50% - 290px/2 + 30.5px);
 background: #E0E8DD;
 border-radius: 30px;
 
+${EmployeeCardContainerStyled}:hover & {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 145px 25px 25px;
+
+    position: absolute;
+    width: 285px;
+    height: 356px;
+    left: calc(50% - 285px/2);
+    top: calc(50% - 356px/2 + 34.5px);
+
+    background: #E0E8DD;
+
+    /* Card_Equipo */
+    box-shadow: 0px 4px 18px rgba(183, 194, 184, 0.22);
+    border-radius: 30px;}
+
 `;
 
 export const EmployeeCardImageStyled = styled.div<{photo:string}>`
@@ -46,11 +65,17 @@ width: 180px;
 height: 180px;
 left: calc(50% - 130px/2 + 0.32px);
 top: calc(50% - 160px/2 - 95.5px);
-
-
- z-index:3;
+z-index:3;
 
 background: url(${({photo}) =>photo});
+
+${EmployeeCardContainerStyled}:hover & {
+    display: flex;
+    position: absolute;
+    width: 179.6px;
+    height: 180px;
+    left: calc(50% - 125.6px/2 + 0.3px);
+    top: calc(50% - 180px/2 - 122.5px);}
 
 `;
 
@@ -73,6 +98,25 @@ flex: none;
 order: 0;
 flex-grow: 0;
 margin: 16px 0px;
+
+${EmployeeCardContainerStyled}:hover & {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+
+    position: static;
+    width: 217px;
+    height: 49px;
+    left: 34px;
+    top: 165.5px;
+
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 16px 0px;}
 
 `;
 
@@ -101,6 +145,30 @@ order: 0;
 flex-grow: 0;
 margin: 0px 0px;
 
+${EmployeeCardContainerStyled}:hover & {
+    position: static;
+    width: 217px;
+    height: 28px;
+    left: 0px;
+    top: 0px;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 28px;
+    text-align: center;
+
+    /* Grises/negro carbon */
+    color: #1C1C1F;
+
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 0px 0px;}
+
 `;
 export const EmployeeCardTextPositionStyled = styled.h3`
 
@@ -126,6 +194,29 @@ flex: none;
 order: 1;
 flex-grow: 0;
 margin: 0px 0px;
+${EmployeeCardContainerStyled}:hover & {
+    position: static;
+    width: 217px;
+    height: 21px;
+    left: 0px;
+    top: 28px;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 21px;
+    text-align: center;
+
+    /* Primario/MALVA */
+    color: #7F516A;
+
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+    margin: 0px 0px;}
 
 `;
 export const EmployeeCardTextStyled = styled.p`
@@ -154,5 +245,30 @@ flex: none;
 order: 1;
 flex-grow: 0;
 margin: 16px 0px;
+${EmployeeCardContainerStyled}:hover & {
+    position: static;
+    width: 217px;
+    height: 80px;
+    left: 34px;
+    top: 230.5px;
+
+    font-family: Inter;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 20px;
+
+    /* or 125% */
+    text-align: center;
+
+    /* Grises/negro carbon */
+    color: #1C1C1F;
+
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+    margin: 16px 0px;}
 
 `;
