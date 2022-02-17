7d5cc88796a3f5e6f5d9afb30bf875d9c330708f
Alcazar87 <carlosalcazar@indotem.com>
2022-02-17 16:19:26 +0100

    Update card component

---
+++ src/components/Cards/Employee_card/EmployeeCardsData.ts
@@ -0,0 +1,32 @@
+
+import Photo from"../../../sample/ExamplePhoto.png"
+import Photo2 from"../../../sample/jaime.png"
+
+export const data = [
+{ 
+ photo:Photo,
+ name:"Jaime López",
+ position:"CEO Indotem",
+ text:'"Indotem nace como un proyecto para ayudar a las personas que más quieres, a ',
+ text2:'tu familia"'
+},
+{ 
+ photo:Photo2,
+ name:"Jaime López",
+ position:"CEO Indotem",
+ text:'"“Ellos se han esforzado desde que nacimos en darnos lo mejor, es el momento de devolverles una parte de lo que se merecen”',
+},
+{ 
+ photo:Photo2,
+ name:"Jaime López",
+ position:"CEO Indotem",
+ text:'"Indotem nace como un proyecto para ayudar a las personas que más quieres, a ',
+ text2:'tu familia"'
+},
+{ 
+ photo:Photo,
+ name:"Jaime López",
+ position:"CEO Indotem",
+ text:'"“Ellos se han esforzado desde que nacimos en darnos lo mejor, es el momento de devolverles una parte de lo que se merecen”',
+}
+]
