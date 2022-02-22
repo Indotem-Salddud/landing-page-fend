e310d67b0d61f6e69dc77dbd613431e2ffb276ee
Alcazar87 <carlosalcazar@indotem.com>
2022-02-22 08:35:12 +0100

    Add new employees

--- src/components/Cards/Employee_card/EmployeeCardsData.ts
+++ src/components/Cards/Employee_card/EmployeeCardsData.ts
@@ -1,32 +1,58 @@
 
-import Photo from"../../../sample/ExamplePhoto.png"
-import Photo2 from"../../../sample/jaime.png"
+import PhotoExample from"../../../resources/pictures/ExamplePhoto.png"
+import PhotoJaime from"../../../resources/pictures/jaime.png"
+import PhotoManu from"../../../resources/pictures/manum.png"
+import PhotoDaniB from"../../../resources/pictures/danielb.png"
 
 export const data = [
 { 
- photo:Photo,
+ photo:PhotoExample,
  name:"Jaime López",
  position:"CEO Indotem",
  text:'"Indotem nace como un proyecto para ayudar a las personas que más quieres, a ',
  text2:'tu familia"'
 },
 { 
- photo:Photo2,
+ photo:PhotoJaime,
  name:"Jaime López",
  position:"CEO Indotem",
- text:'"“Ellos se han esforzado desde que nacimos en darnos lo mejor, es el momento de devolverles una parte de lo que se merecen”',
+ text:'"Ellos se han esforzado desde que nacimos en darnos lo mejor, es el momento de devolverles una parte de lo que se merecen"',
 },
 { 
- photo:Photo2,
+ photo:PhotoManu,
+ name:"Emanuel Martínez",
+ position:"CTO Indotem",
+ text:'"Indotem propone una solución profesional y eficiente para eliminar la soledad de nuestros mayores”',
+},
+{ 
+ photo:PhotoDaniB,
+ name:"Daniel Bañobre",
+ position:"Bend Lead",
+ text:'"Trabajar en Indotem me permite crecer profesionalmente y formar parte de un servicio necesario para muchas personas”',
+},
+{ 
+ photo:PhotoExample,
  name:"Jaime López",
  position:"CEO Indotem",
  text:'"Indotem nace como un proyecto para ayudar a las personas que más quieres, a ',
  text2:'tu familia"'
 },
 { 
- photo:Photo,
+ photo:PhotoJaime,
  name:"Jaime López",
  position:"CEO Indotem",
- text:'"“Ellos se han esforzado desde que nacimos en darnos lo mejor, es el momento de devolverles una parte de lo que se merecen”',
+ text:'"Ellos se han esforzado desde que nacimos en darnos lo mejor, es el momento de devolverles una parte de lo que se merecen”',
+},
+{ 
+ photo:PhotoManu,
+ name:"Emanuel Martínez",
+ position:"CTO Indotem",
+ text:'"Indotem propone una solución profesional y eficiente para eliminar la soledad de nuestros mayores” ',
+},
+{ 
+ photo:PhotoDaniB,
+ name:"Daniel Bañobre",
+ position:"Bend Lead",
+ text:'"Trabajar en Indotem me permite crecer profesionalmente y formar parte de un servicio necesario para muchas personas"',
 }
 ]
