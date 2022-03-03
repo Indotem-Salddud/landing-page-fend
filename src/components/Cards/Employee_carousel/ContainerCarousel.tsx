c29e0a9ee6d508f95690c7f9229849203a0ea14d
Alcazar87 <carlosalcazar@indotem.com>
2022-03-03 19:58:51 +0100

    Update Carusel responsive

--- src/components/Cards/Employee_carousel/ContainerCarousel.tsx
+++ src/components/Cards/Employee_carousel/ContainerCarousel.tsx
@@ -28,21 +28,142 @@ export default function CarouselCont(){
     
   }, [pos])
   let images = [test[pos],test[pos+1],test[pos+2],test[pos+3]];
+  let imagesSm = [test[pos],test[pos+1]];
+  let imagesMobile = test[pos];
+
+  let showPc = document.getElementById("pc");
+  let showPcSmall = document.getElementById("pc-sm");
+  let showMobile = document.getElementById("mobile");
+  
+  console.log(showPc);
+  console.log(showPcSmall);
+  console.log(showMobile)
+
   return (
-    <ContainerStyled >
-      <CardContainer id="container">
+   
+    <ContainerStyled>
+       <CaruselLeftButton id="pc_small_button_left" background={lArrow}
+      onClick={() => {
+        if(pos !== 0){
+          showPcSmall?.classList.add("animateLeft");          
+          setTimeout(() => {showPcSmall?.classList.remove("animateLeft")},1000)
+          setTimeout(() => {showPcSmall?.classList.remove("animateRight")},1000)
+        } 
+        
+        if (pos<=2) {
+          setPos(pos=0);
+          
+          return
+      }
+      setPos(pos - 2);}}/>
+
+      <CaruselLeftButton id="mobile_button_left" background={lArrow}
+      onClick={() => {
+        if(pos !== 0){
+          showMobile?.classList.add("animateLeft");          
+          setTimeout(() => { showMobile?.classList.remove("animateLeft")},1000)
+          setTimeout(() => { showMobile?.classList.remove("animateRight")},1000)
+        } 
+        
+        if (pos<1) {
+          setPos(pos=0);
+          
+          return
+      }
+      setPos(pos - 1);}}/>
+
+     <CaruselLeftButton id="pc_button_left" background={lArrow}
+      onClick={() => {
+        if(pos !== 0){
+          showPc?.classList.add("animateLeft");          
+          setTimeout(() => {showPc?.classList.remove("animateLeft")},1000)
+          setTimeout(() => {showPc?.classList.remove("animateRight")},1000)
+        } 
+        
+        if (pos<=4) {
+          setPos(pos=0);
+          
+          return
+      }
+      setPos(pos - 4);
+    }
+  
+    } 
+      />
+      <CardContainer id="pc">
+        
         {images}
 
       </CardContainer>
 
-      <CaruselLeftButton background={lArrow}
-      onClick={() => {if (pos<=4) {
-          setPos(pos=0) ;return
-      }setPos(pos - 4);}} />
-      <CaruselRightButton background={rArrow}
-      onClick={() => {if (pos+3>=test.length-4) {
-          setPos(pos=test.length-4);return
-      } setPos(pos + 4);}} />
+       <CardContainer id="pc-sm">
+        
+        {imagesSm}
+
+      </CardContainer>
+
+      <CardContainer id="mobile">
+        
+        {imagesMobile}
+
+      </CardContainer>
+
+      
+
+      <CaruselRightButton id="pc_small_button_right" background={rArrow}
+      onClick={() => {
+        if(pos !== test.length - 2){
+          showPcSmall?.classList.add("animateRight");
+          setTimeout(() => {showPcSmall?.classList.remove("animateLeft")},1000)
+          setTimeout(() => {showPcSmall?.classList.remove("animateRight")},1000)
+           
+        } 
+        if (pos+1>=test.length-2) {
+          setPos(pos=test.length-2);
+          
+          return
+      } setPos(pos + 2);
+      }
+      }
+      
+      />
+         <CaruselRightButton id="mobile_button_right" background={rArrow}
+      onClick={() => {
+        if(pos !== test.length - 1){
+          showMobile?.classList.add("animateRight");
+          setTimeout(() => {showMobile?.classList.remove("animateLeft")},1000)
+          setTimeout(() => {showMobile?.classList.remove("animateRight")},1000)
+           
+        } 
+        if (pos+1>=test.length-1) {
+          setPos(pos=test.length-1);
+          
+          return
+      } setPos(pos + 1);
+      }
+      }
+      
+      />
+      <CaruselRightButton id="pc_button_right" background={rArrow}
+  
+      onClick={() => {
+        if(pos !== test.length - 4){
+          showPc?.classList.add("animateRight");
+          setTimeout(() => {showPc?.classList.remove("animateLeft")},1000)
+          setTimeout(() => {showPc?.classList.remove("animateRight")},1000)
+           
+        } 
+        if (pos+3>=test.length-4) {
+          setPos(pos=test.length-4);
+          
+          return
+      } setPos(pos + 4);
+      
+            }
+      
+      }
+      
+      />
      </ContainerStyled>
 
   )
--- src/sample/SampleView.tsx
+++ src/sample/SampleView.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import "./SampleStyle.css";
 import CarouselCont from "../components/Cards/Employee_carousel/ContainerCarousel";
+import "./SampleStyle.css";
+
 
 /**
  * ! Sample view project
@@ -11,16 +12,14 @@ import CarouselCont from "../components/Cards/Employee_carousel/ContainerCarouse
 
 const SampleView: React.FC = () => {
   return (
-    <>
       
       <div className="mt-3">
         <h1>Cards component</h1>
-        <div className="mt-3">
         <CarouselCont />
            
         </div>
-      </div>
-    </>
+
+    
   );
 };
 
